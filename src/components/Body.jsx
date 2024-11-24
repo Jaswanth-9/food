6755072464ad66fd83f201bf3d801ef572881f0a
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2024-11-24 22:12:30 +0530

    fodtile

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./Body.css";
 import Foodcard from "./Foodcard";
 
-const BASE_URL = "http://localhost:9000/";
+export const BASE_URL = "http://localhost:9000";
 
 export default function Body() {
   const [data, setdata] = useState(null);
--- src/components/Foodcard.jsx
+++ src/components/Foodcard.jsx
@@ -1,11 +1,23 @@
+import { BASE_URL } from "./Body";
 import "./Foodcard.css";
 
 export default function Foodcard({ data }) {
   return (
     <div className="foodContainer">
       <foodCard className="foodCard">
-        {data?.map((food) => (
-          <foodtile key={food.name}>{food.text}</foodtile>
+        {data?.map(({ name, image, text, price }) => (
+          <foodtile className="foodTile" key={name}>
+            <div className="food_basic">
+              <div className="food_image">
+                <img src={BASE_URL + image} />
+              </div>
+              <div className="food_info">
+                <h3>{name}</h3>
+                <p>{text}</p>
+              </div>
+            </div>
+            <button>${price.toFixed(2)}</button>
+          </foodtile>
         ))}
       </foodCard>
     </div>
