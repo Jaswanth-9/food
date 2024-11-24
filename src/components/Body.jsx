99958be43eee25b746da2452da6e88d91bb1ed4e
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2024-11-24 11:58:23 +0530

    Body started

---
+++ src/components/Body.jsx
@@ -0,0 +1,10 @@
+import "./Body.css";
+
+export default function Body() {
+  return (
+    <mainBody className="mainBody">
+      <img src="/images/food.jpg" alt="background" />
+      <foodCards></foodCards>
+    </mainBody>
+  );
+}
